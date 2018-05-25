27b3dcb6fae2c72a071ae26b0db5b5df7f5d86eb
David Parra Perez <david.parra@outlook.com>
2018-05-25 10:30:09 +0200

    Fix mixing url in wget

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-wget $REMOTE_default -O /opt/app-root/src/conf/tmp.txt
+wget $REMOTE_APPSETTINGS -O /opt/app-root/src/conf/tmp.txt
 
 md5sum /opt/app-root/src/conf/tmp.txt > /opt/app-root/src/conf/tmp.txt.md5
 
